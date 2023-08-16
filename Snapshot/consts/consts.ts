cf58b46112095661bcb5f979464c5d4e802a20d3
Igor Santos <inascimentodossa@apple.com>
2023-08-16 12:23:48 -0300

    Removing figma token

--- Snapshot/consts/consts.ts
+++ Snapshot/consts/consts.ts
@@ -1,5 +1,5 @@
 export const FIGMA_ID = 'XQOznCEf9YgqLGFIBou010'
-export const TOKEN = 'figd_gl1u0MYJE6Pfw619EDkoNxWIzbJ0BxqH8HnRmEMC'
+export const TOKEN = 'XXXXXXXXXXXXXXXXXXX'
 export const LOGIN_NODE = '1-2'
 export const LOGIN_IMAGE_NAME = 'login.png'
 export const PRODUCT_NODE = '108-227'
